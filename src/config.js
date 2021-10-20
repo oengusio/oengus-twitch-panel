d7be5aaee4d71cbc520b68f248a199eb3ad817d4
duncte123 <contact@duncte123.me>
2021-10-20 16:15:36 +0200

    Ofc this makes sense

---
+++ src/config.js
@@ -0,0 +1,25 @@
+const marathonInput = document.getElementById('marathon');
+
+function saveConfig() {
+    twitch.configuration.set('broadcaster', '1.0', JSON.stringify({
+        marathonId: marathonInput.value,
+    }));
+
+    bulmaToast.toast({
+        duration: 2000,
+        message: 'Config saved!',
+        type: 'is-success',
+        position: 'top-center',
+        dismissible: true,
+    });
+}
+
+function disableExtension() {
+    marathonInput.value = '';
+
+    saveConfig();
+}
+
+loadConfig((config) => {
+    marathonInput.value = config.marathonId || '';
+});
---
+++ src/index.js
@@ -0,0 +1,25 @@
+let interval = -1;
+
+loadConfig((config) => {
+    if (interval > -1) {
+        interval = -1;
+        clearInterval(interval);
+    }
+
+    if (!config.marathonId) {
+        // reset the link in case of a config update
+        document.getElementById('schedule-link').setAttribute('href', 'https://oengus.io/');
+        document.querySelector('.container').innerHTML = '<p>Extension not configured!</p>' +
+            '<p>Please configure the extension within the settings.</p>';
+        return;
+    }
+
+    window.marathonId = config.marathonId;
+
+    updateTicker();
+
+    // update every 60 seconds
+    interval = setInterval(() => {
+        redrawTicker();
+    }, 60 * 1000);
+});
