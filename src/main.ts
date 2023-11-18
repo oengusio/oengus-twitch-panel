9ed7b2405f5a9a36c137ea42954be0e76419d82b
duncte123 <contact@duncte123.me>
2023-11-18 09:58:48 +0100

    Remove google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { useConfigStore } from '@/stores/config';
 
-import './analytics';
-
 import App from './App.vue';
 import router from './router';
 
