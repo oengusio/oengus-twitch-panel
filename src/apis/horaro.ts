9d9ebffad026cd9cf9647e9115c1271950421adf
duncte123 <contact@duncte123.me>
2024-07-21 19:59:11 +0200

    Fix horaro loading

--- src/apis/horaro.ts
+++ src/apis/horaro.ts
@@ -2,7 +2,13 @@ import type { HoraroResponse, HoraroTickerData } from '@/types';
 
 class HoraroApi {
   private get apiBase(): string {
-    return 'https://app.esamarathon.com/horaro-proxy/api_proxy/https://horaro.org/-/api/v1';
+    return 'https://app.esamarathon.com/horaro-proxy/api_proxy/';
+  }
+
+  private horaroUrl(path: string): string {
+    return `${this.apiBase}${encodeURIComponent(
+      `https://horaro.org/-/api/v1${path}`
+    )}`;
   }
 
   public async loadBasicScheduleInfo(
@@ -10,13 +16,15 @@ class HoraroApi {
     scheduleSlug: string
   ): Promise<HoraroTickerData> {
     const res = await fetch(
-      `${this.apiBase}/events/${event}/schedules/${scheduleSlug}/ticker`,
+      this.horaroUrl(`/events/${event}/schedules/${scheduleSlug}/ticker`),
       {
         headers: {
           'User-Agent': 'OengusIO Twitch Panel',
         },
       }
     );
+
+    console.log(res);
 
     if (res.status === 404) {
       throw new Error('Event or schedule not found');
@@ -30,7 +38,7 @@ class HoraroApi {
   public async getTickerData(
     scheduleId: string
   ): Promise<HoraroTickerData | null> {
-    const res = await fetch(`${this.apiBase}/schedules/${scheduleId}/ticker`, {
+    const res = await fetch(this.horaroUrl(`/schedules/${scheduleId}/ticker`), {
       headers: {
         'User-Agent': 'OengusIO Twitch Panel',
       },
